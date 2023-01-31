2de51aeadfde680d7f52e6247fef8c0f22b0a0b8
quardianwolf <quardianwolf@gmail.com>
2023-01-31 19:26:10 +0300

    fix

--- pages/profile.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Minimal, WithLayout } from "../src/layouts";
-import Profile from "../src/views/Profile";
-
-const ProfilePage = () => {
-  return <WithLayout component={Profile} layout={Minimal} />;
-};
-
-export default ProfilePage;
